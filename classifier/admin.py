b7c93b79748862fc98047fbd31d6ce0d127378ba
Hans Tananda <hanstananda@gmail.com>
2021-02-04 02:26:06 +0800

    register models to admin site

--- classifier/admin.py
+++ classifier/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from classifier.models import SnakeImage, ClassifySnakeRequest, SnakeInfo
+
+admin.site.register(SnakeImage)
+admin.site.register(SnakeInfo)
+admin.site.register(ClassifySnakeRequest)
